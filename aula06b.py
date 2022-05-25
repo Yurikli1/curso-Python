157a9c2f56ee51476488dd77dd643bdb9f3f8566
Yurikli1 <yuriklissmann@hotmail.com>
2022-05-25 19:05:34 -0300

    Atualizando Repositório Python

---
+++ aula06b.py
@@ -0,0 +1,13 @@
+n1= int(input('numero : '))
+pri= n1 * 1
+seg= n1 * 2
+ter= n1 * 3
+quar= n1 * 4
+qui= n1 * 5
+sex= n1 * 6
+set= n1 * 7
+oit= n1 * 8
+non= n1 * 9
+dec= n1 * 10
+print('o resultado da  tabuada é= \n resultado por 1: {} \n resultado por 2: {} \n resultado por 3: {} \n resultado por 4: {} \n resultado por 5: {} \n resultado por 6: {} \n resultado por 7: {} \n resultado por 8: {} \n resultado por 9: {} \n resultado por 10: {} ' .format( pri, seg, ter, quar, qui, sex, set, oit, non, dec))
+
---
+++ ex002.py
@@ -0,0 +1,1 @@
+print=('Como é seu nome?')
